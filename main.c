33d9edfbc7b611ec54369bd3b5a2b835c91055df
Takubon Son <son@Takubons-MacBook-Pro.local>
2020-11-21 10:08:18 +0900

    更新しました

--- main.c
+++ main.c
@@ -4,16 +4,18 @@
 #define SIZE 4
 int random_in_range(int);
 void print_board(int [SIZE][SIZE]);
+int check_if_full(int [SIZE][SIZE]);
 void put_random_2_in(int [SIZE][SIZE]);
 void start_game(int [SIZE][SIZE]);
-void move_up(int [SIZE][SIZE]);
-void move_down(int [SIZE][SIZE]);
-void move_left(int [SIZE][SIZE]);
-void move_right(int [SIZE][SIZE]);
+void move_up(int [SIZE][SIZE], int *);
+void move_down(int [SIZE][SIZE], int *);
+void move_left(int [SIZE][SIZE], int *);
+void move_right(int [SIZE][SIZE], int *);
 
 
 int main (int argc, char *argv[]) {
     int board[SIZE][SIZE] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+    int score = 0;
     printf("Press any key to conitnue\n");
     int c;
     system("clear");
@@ -26,26 +28,32 @@ int main (int argc, char *argv[]) {
     while((c = getchar()) != '.') {
         system("clear");
         printf("Press '.' to close\r\n");
-        if (c == 'A'){
-            move_up(board);
+        if (check_if_full(board)){
+            system("clear");
+            print_board(board);
+            printf("\r\n\r\nGAME OVER !\r\n\r\n");
+            printf("Your Score: %d\r\n", score);
+            break;
+        }else if (c == 'A')
+        {
+            move_up(board, &score);
             put_random_2_in(board);
         }else if (c == 'D')
         {
-            move_left(board);
+            move_left(board, &score);
             put_random_2_in(board);
         }else if (c == 'B')
         {
-            move_down(board);
+            move_down(board, &score);
             put_random_2_in(board);
         }else if (c == 'C')
         {
-            move_right(board);
+            move_right(board, &score);
             put_random_2_in(board);
         }
         print_board(board);
-        printf("You pressed '%c'\r\n", c);
+        printf("Your Score: %d\r\n", score);
     }
-
     system("/bin/stty cooked");  // 後始末
 
     return 0;
@@ -79,14 +87,32 @@ void print_board(int board[SIZE][SIZE]) {
     }
 }
 
+int check_if_full(int board[SIZE][SIZE]){
+    for (int i=0; i<SIZE; i++){
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (board[i][j] == 0){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
 void put_random_2_in(int board[SIZE][SIZE]){
     int flag = 0;
+
     while(flag == 0){
-        int m = random_in_range(SIZE);
-        int n = random_in_range(SIZE);
-        if (board[m][n] == 0){
-            board[m][n] = 2;
-            flag = 1;
+        if(check_if_full(board)){
+            break;
+        }else
+        {
+            int m = random_in_range(SIZE);
+            int n = random_in_range(SIZE);
+            if (board[m][n] == 0){
+                board[m][n] = 2;
+                flag = 1;
+            }
         }
     }
 }
@@ -96,8 +122,8 @@ void start_game(int board[SIZE][SIZE]){
     put_random_2_in(board);
 }
 
-void move_up(int board[SIZE][SIZE]){
-    int flag[SIZE] = {0, 0, 0, 0}; //各列の足し算は一回までなので
+void move_up(int board[SIZE][SIZE], int *s){
+    int flag[SIZE] = {0, 0, 0, 0}; //各列の足し算は一回まで
     for (int j=0; j<SIZE; j++){
         for (int i=1; i<SIZE; i++){
             if (board[i][j] != 0){
@@ -109,6 +135,7 @@ void move_up(int board[SIZE][SIZE]){
                     {
                         if (flag[j]==0){
                             board[k][j] *= 2;
+                            *s += board[k][j];
                             board[k+1][j] = 0;
                             flag[j] = 1;
                         }
@@ -119,8 +146,8 @@ void move_up(int board[SIZE][SIZE]){
     }
 }
 
-void move_down(int board[SIZE][SIZE]){
-    int flag[SIZE] = {0, 0, 0, 0}; //各列の足し算は一回までなので
+void move_down(int board[SIZE][SIZE], int *s){
+    int flag[SIZE] = {0, 0, 0, 0}; //各列の足し算は一回まで
     for (int j=0; j<SIZE; j++){
         for (int i = SIZE-2; i>=0; i--){
             if (board[i][j]!=0){
@@ -132,6 +159,7 @@ void move_down(int board[SIZE][SIZE]){
                     {
                         if (flag[j]==0){
                             board[k][j]*=2;
+                            *s += board[k][j];
                             board[k-1][j]=0;
                             flag[j] = 1;
                         }
@@ -142,8 +170,8 @@ void move_down(int board[SIZE][SIZE]){
     }
 }
 
-void move_left(int board[SIZE][SIZE]){
-    int flag[SIZE] = {0, 0, 0, 0}; //各行の足し算は一回までなので
+void move_left(int board[SIZE][SIZE], int *s){
+    int flag[SIZE] = {0, 0, 0, 0}; //各行の足し算は一回まで
     for (int i=0; i<SIZE; i++){
         for (int j=1; j<SIZE; j++){
             if (board[i][j] != 0){
@@ -155,6 +183,7 @@ void move_left(int board[SIZE][SIZE]){
                     {
                         if(flag[i]==0){
                             board[i][k]*=2;
+                            *s += board[i][k];
                             board[i][k+1]=0;
                             flag[i]=1;
                         }
@@ -165,7 +194,7 @@ void move_left(int board[SIZE][SIZE]){
     }
 }
 
-void move_right(int board[SIZE][SIZE]){
+void move_right(int board[SIZE][SIZE], int *s){
     int flag[SIZE] = {0, 0, 0, 0};
     for (int i=0; i<SIZE; i++){
         for(int j=SIZE-2; j>=0; j--){
@@ -178,6 +207,7 @@ void move_right(int board[SIZE][SIZE]){
                     {
                         if (flag[i] == 0){
                             board[i][k] *= 2;
+                            *s += board[i][k];
                             board[i][k-1] = 0;
                             flag[i]=1;
                         }
